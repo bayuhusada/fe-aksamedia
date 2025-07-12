0c64dbac7b65a1a5b433614e79e24af41072bb04
bayuhusada <nekobay08@gmail.com>
2025-07-12 16:48:00 +0800

    First Commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900'>
+      <form className="bg-white dark:bg-gray-800 p-8 rounded-lg shadow-md w-full max-w-sm" action="" method="">
+        <h2 className="text-2xl font font-semibold text-center text-gray-800 dark:text-white mb-4"></h2>
+      </form>
+    </div>
+  )
+}
+
+export default Login
